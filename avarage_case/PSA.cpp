6d5a7241384c2843a09859f6cc3b63fd824d3e69
YagaoLiu <chang.liu.kcl@gmail.com>
2018-06-28 17:26:38 +0100

    avarage_case

---
+++ avarage_case/PSA.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vecter>
+#include <cmath>
+#include <bitset>
+
+using namespace std;
+
+vector<int>& signature( string& text, vector<int>& property, string& alphabet ){
+	sigma = alphabet.size() + 1;
+	double t = floor( log(text.size()) / log(sigma) );
+	vector<int> integer_text ( text.size() + t );
+	vector<int> sign ( text.size() );
+	for ( int i = 0; i < text.size(); i++ ){
+		integer_text[i] = alphabet.find( text[i] ) + 1;
+	}
+	for ( int i = text.size(); i < integer_text; i++ ){
+		integer_text[i] = 0;
+	}
+
+	sign[0] = 0;
+	for ( int j = 1; j < t; j++ ){
+		sign[0] += integer_text[j-1] * pow(sigma, t-j);
+	}
+	for ( int i = 1; i < text.size(); i++ ){
+		sign[i] = ( sign[i-1] - integer_text[i-1] * pow(sigma, t-1) ) * sigma + integer_text[i+t-1];
+	}
+	for ( int i = 0; i < text.size(); i++ ){
+		if ( property[i] < t ){
+			sign[i] = sign[i]&( pow(sigma, t) - pow(sigma, t-property[i]) );
+		}
+	}
+	return sign;
+}
+
